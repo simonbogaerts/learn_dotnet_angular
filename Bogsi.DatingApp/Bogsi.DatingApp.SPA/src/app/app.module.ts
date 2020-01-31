8916379db69cbb1f93c381524e77b824ec8e5c83
simonbogaerts <simon.bogaerts@outlook.com>
2020-01-31 16:57:44 +0100

    navbar

--- Bogsi.DatingApp/Bogsi.DatingApp.SPA/src/app/app.module.ts
+++ Bogsi.DatingApp/Bogsi.DatingApp.SPA/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './components/value/value.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
